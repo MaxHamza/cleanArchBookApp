db280e9a3974b5604725206b3f17a48766a954bb
Hamza <hamza1mks1@gmail.com>
2023-10-22 13:39:14 +0300

    fix

---
+++ clean_arch_bookly_app-09a30c52ab53e745ff8874947dfeea1550ce4efd/lib/Features/home/domain/entities/home_entity.dart
@@ -0,0 +1,8 @@
+class HomeEntity{
+  final String image;
+  final String author;
+  final String title;
+  final num price;
+  final num rating;
+  HomeEntity(this.image, this.author, this.title, this.price, this.rating);
+}
