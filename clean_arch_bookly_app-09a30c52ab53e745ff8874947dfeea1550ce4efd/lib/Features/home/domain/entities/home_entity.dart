aca05d878d5dbbd34c0fc594f3347965797067d0
Hamza <hamza1mks1@gmail.com>
2023-10-26 01:09:55 +0300

    create hive

--- clean_arch_bookly_app-09a30c52ab53e745ff8874947dfeea1550ce4efd/lib/Features/home/domain/entities/home_entity.dart
+++ clean_arch_bookly_app-09a30c52ab53e745ff8874947dfeea1550ce4efd/lib/Features/home/domain/entities/home_entity.dart
@@ -1,9 +1,19 @@
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+part 'home_entity.g.dart';
+@HiveType(typeId:0)
 class BookEntity{
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String ?image;
+  @HiveField(2)
   final String ?author;
+  @HiveField(3)
   final String title;
+  @HiveField(4)
   final num ?price;
+  @HiveField(5)
   final num ?rating;
   BookEntity({required this.image,required this.author,required this.title,required this.price,required this.rating,required this.bookId});
 }
